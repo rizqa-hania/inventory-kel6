8fa4d632d6a5d23fd6a6c68a0d019236a05bf3df
Aqilla14 <aqilla.oktaviani214@smk.belajar.id>
2026-08-06 13:10:33 +0700

    controller jenis_barang

---
+++ app/Http/Controllers/JenisController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Jenis;
+use App\Barang;
+
+class JenisController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $jenis = Jenis::all();
+        return view('jenis.index', compact('jenis')); 
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('jenis.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'jenis_barang' => 'required'
+        ]);
+
+        Jenis::create([
+            'jenis_barang' => $request->jenis_barang
+        ]);
+
+        return redirect()->route('jenis.index')
+                     ->with('success', 'Data berhasil ditambahkan');    
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $jenis = Jenis::findOrFail($id);
+
+        return view('jenis.edit', compact('jenis'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'jenis_barang' => 'required'
+        ]);
+
+        $jenis = Jenis::findOrFail($id);
+
+        $jenis->update([
+            'jenis_barang' => $request->jenis_barang
+        ]);
+
+        return redirect()->route('jenis.index')
+                     ->with('success', 'Data berhasil diubah');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $jenis = Jenis::findOrFail($id);
+
+        $jenis->delete();
+
+        return redirect()->route('jenis.index')
+                     ->with('success', 'Data berhasil dihapus');
+    }
+}
--- app/Jenis.php
+++ app/Jenis.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Jenis extends Model
 {
-    //
+    protected $table = 'jenis';
+    protected $primaryKey = 'jenis_id';
+    protected $fillable = ['jenis_barang'];
+
+    public function jenis()
+    {
+        return $this->hasMany(Jenis::class, 'jenis_id', 'jenis_id');
+    }
 }
