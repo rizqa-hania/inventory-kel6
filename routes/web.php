2aa0515beca4154bddb5c51112f5dbc92d24f3c4
adiniarasya <adinia.rasya28@smk.belajar.id>
2026-08-06 11:45:20 +0700

    menambahkan template Admin LTE dan route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\BarangController;
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\RuangController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,9 +20,26 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/ruang', [RuangController::class, 'index'])->name('ruang.index'); // Tampilkan semua kategori
-Route::get('/ruang/create', [RuangController::class, 'create'])->name('ruang.create'); // Form tambah kategori
-Route::post('/ruang', [RuangController::class, 'store'])->name('ruang.store'); // Simpan kategori baru
-Route::get('/ruang/{id}/edit', [RuangController::class, 'edit'])->name('ruang.edit'); // Form edit kategori
-Route::put('/ruang/{id}', [RuangController::class, 'update'])->name('ruang.update'); // Update kategori
-Route::delete('/ruang/{id}', [RuangController::class, 'destroy'])->name('ruang.destroy'); // Hapus kategori
+// Ruang
+Route::get('/ruang', [RuangController::class, 'index'])->name('ruang.index'); // Tampilkan semua ruang
+Route::get('/ruang/create', [RuangController::class, 'create'])->name('ruang.create'); // Form tambah ruang
+Route::post('/ruang', [RuangController::class, 'store'])->name('ruang.store'); // Simpan ruang baru
+Route::get('/ruang/{id}/edit', [RuangController::class, 'edit'])->name('ruang.edit'); // Form edit ruang
+Route::put('/ruang/{id}', [RuangController::class, 'update'])->name('ruang.update'); // Update ruang
+Route::delete('/ruang/{id}', [RuangController::class, 'destroy'])->name('ruang.destroy'); // Hapus ruang
+
+// Kategori
+Route::get('/kategori', [KategoriController::class, 'index'])->name('kategori.index'); // Tampilkan semua kategori
+Route::get('/kategori/create', [KategoriController::class, 'create'])->name('kategori.create'); // Form tambah kategori
+Route::post('/kategori', [KategoriController::class, 'store'])->name('kategori.store'); // Simpan kategori baru
+Route::get('/kategori/{id}/edit', [KategoriController::class, 'edit'])->name('kategori.edit'); // Form edit kategori
+Route::put('/kategori/{id}', [KategoriController::class, 'update'])->name('kategori.update'); // Update kategori
+Route::delete('/kategori/{id}', [KategoriController::class, 'destroy'])->name('kategori.destroy'); // Hapus kategori
+
+// Barang
+Route::get('/barang', [BarangController::class, 'index'])->name('barang.index'); // Tampilkan semua barang
+Route::get('/barang/create', [BarangController::class, 'create'])->name('barang.create'); // Form tambah barang
+Route::post('/barang', [BarangController::class, 'store'])->name('barang.store'); // Simpan barang baru
+Route::get('/barang/{id}/edit', [BarangController::class, 'edit'])->name('barang.edit'); // Form edit barang
+Route::put('/barang/{id}', [BarangController::class, 'update'])->name('barang.update'); // Update barang
+Route::delete('/barang/{id}', [BarangController::class, 'destroy'])->name('barang.destroy'); // Hapus barang
