4353296e3f5ea21ae98f94d6effecd2ebb163edc
rizqa-hania <rizqa.hania39@smk.belajar.id>
2026-08-13 01:45:46 +0700

    rizqa

--- resources/views/template/sidebar.blade.php
+++ resources/views/template/sidebar.blade.php
@@ -2,7 +2,7 @@
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
       <img src="{{ asset('images/logo inventory.png')}}" alt="Inventory Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">Inventory Barang</span>
+      <span class="brand-text font-weight-light">Inventoris Barang</span>
     </a>
 
 
