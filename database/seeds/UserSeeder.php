d8b21108e6cb5ad877eb3b6c21c73f7d1f82d766
zaidathalitha29 <zaida.thalitha43@smk.belajar.id>
2026-09-03 08:36:41 +0700

    update data seed

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -2,15 +2,10 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
         DB::table('users')->insert([
@@ -19,35 +14,35 @@ class UserSeeder extends Seeder
                 'username' => 'Lina',
                 'email' => 'maulina.sheva@gmail.com',
                 'no_telp' => '089512345678',
-                'password' => '12345',
+                'password' => Hash::make('12345'),
             ],
             [
                 'nama' => 'Melani Widiastuti',
                 'username' => 'Melani',
                 'email' => 'melani.widiastuti@gmail.com',
                 'no_telp' => '089512344321',
-                'password' => '12345',
+                'password' => Hash::make('12345'),
             ],
             [
                 'nama' => 'Nakilla Khoirin Periska',
                 'username' => 'Nakilla',
                 'email' => 'nakilla.khoirin@gmail.com',
                 'no_telp' => '089598765432',
-                'password' => '12345',
+                'password' => Hash::make('12345'),
             ],
             [
                 'nama' => 'Rizqa Hania Putri Arahman',
                 'username' => 'Rizqa',
                 'email' => 'rizqa.hania@gmail.com',
                 'no_telp' => '089523456789',
-                'password' => '12345',
+                'password' => Hash::make('12345'),
             ],
             [
                 'nama' => 'Zaida Thalitha Atiqah',
                 'username' => 'Zaida',
                 'email' => 'zaida.thalita@gmail.com',
                 'no_telp' => '089511112222',
-                'password' => '12345',
+                'password' => Hash::make('12345'),
             ],
         ]);
     }
