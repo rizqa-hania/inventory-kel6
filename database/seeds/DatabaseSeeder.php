fd9de53b70881d3af9c7b1c2a80a1a452dd2cf3a
zaidathalitha29 <zaida.thalitha43@smk.belajar.id>
2026-09-03 08:51:38 +0700

    update data seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,44 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('users')->insert([
+            [
+                'nama' => 'Maulina Sheva',
+                'username' => 'Lina',
+                'email' => 'maulina.sheva@gmail.com',
+                'no_telp' => '089512345678',
+                'password' => Hash::make('12345'),
+            ],
+            [
+                'nama' => 'Melani Widiastuti',
+                'username' => 'Melani',
+                'email' => 'melani.widiastuti@gmail.com',
+                'no_telp' => '089512344321',
+                'password' => Hash::make('12345'),
+            ],
+            [
+                'nama' => 'Nakilla Khoirin Periska',
+                'username' => 'Nakilla',
+                'email' => 'nakilla.khoirin@gmail.com',
+                'no_telp' => '089598765432',
+                'password' => Hash::make('12345'),
+            ],
+            [
+                'nama' => 'Rizqa Hania Putri Arahman',
+                'username' => 'Rizqa',
+                'email' => 'rizqa.hania@gmail.com',
+                'no_telp' => '089523456789',
+                'password' => Hash::make('12345'),
+            ],
+            [
+                'nama' => 'Zaida Thalitha Atiqah',
+                'username' => 'Zaida',
+                'email' => 'zaida.thalita@gmail.com',
+                'no_telp' => '089511112222',
+                'password' => Hash::make('12345'),
+            ],
+        ]);
+
         DB::table('ruang')->insert([
             [
                 'nama_ruang' => 'Lab 1',
